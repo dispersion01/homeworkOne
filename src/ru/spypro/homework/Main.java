8bc731d33983de2ec3daec62647707e45043cf3d
dispersion <anyta1901@gmail.com>
2021-12-26 01:09:28 +0300

    Задача 2

--- src/ru/spypro/homework/Main.java
+++ src/ru/spypro/homework/Main.java
@@ -5,6 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
+        float boxerOne = 78.2f;
+        float boxerTwo = 82.7f;
 
+        float weightBoxer = boxerOne + boxerTwo;
+        System.out.println(weightBoxer);
+
+        var differentWeight = boxerTwo - boxerOne;
+        System.out.println(differentWeight);
     }
 }
