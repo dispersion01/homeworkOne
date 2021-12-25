144bd559deb96a3259c6d887caca17755136aace
dispersion <anyta1901@gmail.com>
2021-12-26 01:19:53 +0300

    Задача 4

--- src/ru/spypro/homework/Main.java
+++ src/ru/spypro/homework/Main.java
@@ -4,6 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int goalWeight = 7*1000;
+        int weightGoalOne = 250;
+        int weightGoalOTwo = 500;
+
+        /*float goalOne = (float) (weightGoalOne/goalWeight);*/
+        var goalOne =  (goalWeight/weightGoalOne);
+
+        System.out.println("Количество дней, если худеть по 250гр: " + goalOne);
+
+        /*float goalTwo = (float)(weightGoalOTwo/goalWeight) ;*/
+        var goalTwo = (goalWeight/weightGoalOTwo) ;
+        System.out.println("Количество дней, если худеть по 500гр: " +goalTwo);
+
+        var averageDay = (goalOne + goalTwo) /2;
+        System.out.println("Среднее количество дней нужное, чтобы похудеть на 7кг: " + averageDay);
 
 
     }
