e0fa80ff77d7f7b2dd1dfbd14408b86c6f57afa1
dispersion <anyta1901@gmail.com>
2021-12-26 01:07:30 +0300

    Задача 1

--- src/ru/spypro/homework/Main.java
+++ src/ru/spypro/homework/Main.java
@@ -5,6 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-
+        byte numberOne = 3;
+        short numberTwo = -3;
+        int numberThree = 5;
+        long numberFour = 96;
+        float numberFive = 3.14f;
+        double numberSix = -52;
+        char numberSeven = 4;
+        boolean numberEight = true;
     }
 }
