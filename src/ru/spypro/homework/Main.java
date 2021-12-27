5765fd1b0c7e0e016e7ce535e74f15123f96ccb3
dispersion <anyta1901@gmail.com>
2021-12-27 14:23:30 +0300

    Коммит по всем задачам с поправками по решениям

--- src/ru/spypro/homework/Main.java
+++ src/ru/spypro/homework/Main.java
@@ -15,79 +15,105 @@ public class Main {
         byte numberOne = 3;
         short numberTwo = -3;
         int numberThree = 5;
-        long numberFour = 96;
+        long numberFour = 96L;
         float numberFive = 3.14f;
         double numberSix = -52;
         char numberSeven = 4;
         boolean numberEight = true;
     }
     public static void exerciseTwo(){
-        float boxerOne = 78.2f;
-        float boxerTwo = 82.7f;
 
-        float weightBoxer = boxerOne + boxerTwo;
-        System.out.println(weightBoxer);
+            float boxerFirstWeigh = 78.2f;
+            float boxerSecondWeigh = 82.7f;
 
-        var differentWeight = boxerTwo - boxerOne;
-        System.out.println(differentWeight);
+            float totalWeightBoxer = boxerFirstWeigh + boxerSecondWeigh;
+            System.out.println("Общий вес двух бойцов: " + totalWeightBoxer);
+
+            var differentWeight = boxerSecondWeigh - boxerFirstWeigh;
+            System.out.println("Разница между весами бойцов: " + differentWeight);
+
+
 
     }
     public static void exerciseThree(){
+        int grInKg = 1000;
+
         int amountBanana = 80;
         var weightBananas = amountBanana *5;
 
-        int amountMilk = 105;
-        float weightMilk =(200f/100f)*amountMilk;
+        int milkWeightPer100Ml = 105;
+        float weightMilk =(200f/100f)*milkWeightPer100Ml;
 
         int amountIceCream = 100;
-        var iceCreamWeight = amountIceCream *2;
+        int iceCreamWeight = amountIceCream *2;
 
         int amountEgg = 70;
-        var weightEgg = amountEgg *4;
+        int weightEgg = amountEgg *4;
 
-        var recipe = weightBananas + weightMilk + weightEgg + iceCreamWeight;
-        System.out.println(recipe/1000);
+        float recipe = weightBananas + weightMilk + weightEgg + iceCreamWeight;
+        System.out.println("Вес коктейля в кг: " + recipe/grInKg);
+        System.out.println("Вес коктейля в гр: " + (recipe));
     }
     public static void exerciseFour(){
-        int goalWeight = 7*1000;
-        int weightGoalOne = 250;
-        int weightGoalOTwo = 500;
+        /*int goalWeight = 7;
+        double weightGoalOne = 250*0.001;
+        double weightGoalOTwo = 500*0.001;
 
-        /*float goalOne = (float) (weightGoalOne/goalWeight);*/
-        var goalOne =  (goalWeight/weightGoalOne);
+
+        int goalOne = (int) (goalWeight/weightGoalOne);
 
         System.out.println("Количество дней, если худеть по 250гр: " + goalOne);
 
-        /*float goalTwo = (float)(weightGoalOTwo/goalWeight) ;*/
-        var goalTwo = (goalWeight/weightGoalOTwo) ;
+
+       int goalTwo = (int) (goalWeight/weightGoalOTwo);
         System.out.println("Количество дней, если худеть по 500гр: " +goalTwo);
 
-        var averageDay = (goalOne + goalTwo) /2;
+        int averageDay = (goalOne + goalTwo) /2;
+        System.out.println("Среднее количество дней нужное, чтобы похудеть на 7кг: " + averageDay);*/
+        int goalWeight = 7;
+        int grInKg = 1000;
+        int goalWeightInGr = goalWeight * grInKg;
+        int weightGoalOne = 250;
+        int weightGoalOTwo = 500;
+
+
+        int goalOne =  ( goalWeightInGr/weightGoalOne);
+
+        System.out.println("Количество дней, если худеть по 250гр: " + goalOne);
+
+
+        int goalTwo =  ( goalWeightInGr/weightGoalOTwo);
+        System.out.println("Количество дней, если худеть по 500гр: " +goalTwo);
+
+        int averageDay = (goalOne + goalTwo) /2;
         System.out.println("Среднее количество дней нужное, чтобы похудеть на 7кг: " + averageDay);
 
     }
     public static void exerciseFive(){
-        int misha = 67_760;
-        int denis = 83_690;
-        int kristina = 76_230;
+        int mishaSalary = 67_760;
+        int denisSalary = 83_690;
+        int kristinaSalary = 76_230;
 
-        var bonusMisha = (misha * 10)/100;
-        var salaryNewMisha = bonusMisha + misha;
-        var salaryYearOldMisha = misha *12;
+        int percent = 10;
+        float salaryPercent = percent / 100f;
+
+        var bonusMisha = mishaSalary * salaryPercent;
+        var salaryNewMisha = bonusMisha + mishaSalary;
+        var salaryYearOldMisha = mishaSalary *12;
         var salaryYearNewMisha = salaryNewMisha * 12;
-        var salaryYearMisha = salaryYearNewMisha - salaryYearOldMisha;
+        int salaryYearMisha = (int) (salaryYearNewMisha - salaryYearOldMisha);
 
-        var bonusDenis = (denis * 10)/100;
-        var salaryNewDenis = bonusDenis + denis;
-        var salaryYearOldDenis = denis *12;
+        var bonusDenis = denisSalary * salaryPercent;
+        var salaryNewDenis = bonusDenis + denisSalary;
+        var salaryYearOldDenis = denisSalary *12;
         var salaryYearNewDenis = salaryNewDenis * 12;
-        var salaryYearDenis = salaryYearNewDenis - salaryYearOldDenis;
+        int salaryYearDenis = (int) (salaryYearNewDenis - salaryYearOldDenis);
 
-        var bonusKristina = ((kristina * 10)/100);
-        var salaryNewKristina = bonusKristina + kristina;
-        var salaryYearOldKristina = kristina *12;
+        var bonusKristina = kristinaSalary * salaryPercent;
+        var salaryNewKristina = bonusKristina + kristinaSalary;
+        var salaryYearOldKristina = kristinaSalary *12;
         var salaryYearNewKristina = salaryNewKristina * 12;
-        var salaryYearKristina = salaryYearNewKristina - salaryYearOldKristina;
+        int salaryYearKristina = (int) (salaryYearNewKristina - salaryYearOldKristina);
 
         System.out.println("Миша теперь получает " + salaryNewMisha + " рублей." + "Годовой доход вырос на " + salaryYearMisha + " рублей." );
         System.out.println("Денис теперь получает " + salaryNewDenis + " рублей" + "Годовой доход вырос на " + salaryYearDenis + " рублей." );
